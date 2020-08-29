7f914f99bbe83509ee7388bef35b6a31eeb6a182
Byelmanov <yelmanovbohdan@gmail.com>
2020-08-29 18:21:42 +0300

    our_works component in progres

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,6 +2,6 @@ module.exports = function () {
     $.gulp.task('watch', function () {
         $.gulp.watch('src/pug/**/*.pug', $.gulp.series('pug'))
         $.gulp.watch('src/sass/**/*.sass', $.gulp.series('sass'));
-        $.gulp.watch('src/js/main.js', $.gulp.series('scripts'));
+        $.gulp.watch('src/js/*.js', $.gulp.series('scripts'));
     });
 }
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,1 +1,23 @@
-console.log('dqwd');
+$('document').ready(() => {
+    $('#ourWorksSlider').slick({
+        // infinite: true,
+        // slidesToShow: 3,
+        // dots: false,
+        // speed: 1000,
+        // // centerMode: true,
+        // draggable: true,
+        // arrows: false,
+        // variableWidth: true,
+        // responsive: [{
+        //     breakpoint: 1024,
+        //     settings: {
+        //     }
+        // },
+        // ],
+        infinite: true,
+        slidesToShow: 3,
+        dots: false,
+        arrows: false,
+        draggable: true,
+    });
+});
