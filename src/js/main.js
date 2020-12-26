18772a4fedca5b41d311a8826cd24ff4a04f7ebb
Cheewbacca <vanya.timoschenko85@gmail.com>
2020-12-26 20:18:25 +0200

    forms

--- src/js/main.js
+++ src/js/main.js
@@ -92,3 +92,44 @@ $('#cross').on('click', function(e){
 });
 
 $("#consultation_phone").inputmask({"mask": "(+380) 99-999-99-99"});
+
+function sendForm(type){
+    type == 'footerForm' ? type = '' : type = '-modal';
+
+    $.ajax({
+        method: "POST",
+        url: $(this).attr('action'),
+        data: { 
+            name: $('#consultation_name' + type).val(), 
+            phone: $('#consultation_phone' + type).val(),
+        }
+    })
+    .done(function( response ) {
+        
+        if (type){
+            $('.modal__content').children().hide();
+            $('#modal__cross').show();
+        }else{
+            $('#footerForm').children().hide();
+        }
+
+
+        if (response.success == false){
+            $('#error' + type).show();
+        }else{
+            $('#success' + type).show();
+        }
+    });
+}
+
+$('#footerForm').on('submit', function(e){
+    e.preventDefault();
+
+    sendForm('footerForm');
+});
+
+$('#modalForm').on('submit', function(e){
+    e.preventDefault();
+
+    sendForm('modalForm');
+});
